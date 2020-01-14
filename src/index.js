a682eca3d65b2c22fd330d5b401b7aa24b8bb5d9
Aliaksandra Havia <aliaksandrahavia@northwesternmutual.com>
2020-01-14 13:21:18 -0500

    Installed webpack and babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';;
+import ReactDOM from 'react-dom';
+
+const Welcome = () => (<div><h1>Hello World!</h1></div>);
+
+ReactDOM.render(<Welcome />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = { 
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']    
+            }
+        ]
+    }
+};
