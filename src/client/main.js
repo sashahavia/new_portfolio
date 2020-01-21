3671c2086392773d9a3dea98f03062425e420663
Aliaksandra Havia <aliaksandrahavia@northwesternmutual.com>
2020-01-21 15:59:38 -0500

    Code cleanup

---
+++ src/client/main.js
@@ -0,0 +1,6 @@
+import React from 'react';;
+import ReactDOM from 'react-dom';
+
+const Welcome = () => (<h1>Hello World!</h1>);
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';;
-import ReactDOM from 'react-dom';
-
-const Welcome = () => (<div><h1>Hello World!</h1></div>);
-
-ReactDOM.render(<Welcome />, document.getElementById("root"));
