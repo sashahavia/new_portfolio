f72816b8368e8299a6b3ebbb8a3cdbb44ad4ff98
Aliaksandra Havia <aliaksandrahavia@northwesternmutual.com>
2020-02-11 14:34:32 -0500

    fixed deprecationwarning for mongodb

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 8080;
 
 // DB connection
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
